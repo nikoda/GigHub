b0d9cf6c8811987eba5c05e291b9ef4ca5d40b06
James Allen <james_allen.irish@yahoo.com>
2017-12-03 20:50:13 +0100

    Add name to artist and move files to ViewModel folder

--- GigHub/Migrations/201712031923387_AddNameToApplicationUser.Designer.cs
+++ GigHub/Migrations/201712031923387_AddNameToApplicationUser.Designer.cs
@@ -7,13 +7,13 @@ namespace GigHub.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class FixedGenreMisspelling1 : IMigrationMetadata
+    public sealed partial class AddNameToApplicationUser : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(FixedGenreMisspelling1));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(AddNameToApplicationUser));
         
         string IMigrationMetadata.Id
         {
-            get { return "201711301145548_FixedGenreMisspelling1"; }
+            get { return "201712031923387_AddNameToApplicationUser"; }
         }
         
         string IMigrationMetadata.Source
